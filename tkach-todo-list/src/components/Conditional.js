60338444d4919145a1e27a707d38c373ae936e98
thach90 <tkach90@yahoo.com.ua>
2019-04-04 09:24:13 +0300

    conditional state button

--- tkach-todo-list/src/components/Conditional.js
+++ tkach-todo-list/src/components/Conditional.js
@@ -1,15 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 
-function Conditional(props) {
-    return (
-        <Fragment>
-            {props.isLoading ?
-                <h1>Loading...</h1> :
-                <h1>Some important info</h1>
-            }
-        </Fragment>
-    )
+class Conditional extends PureComponent{
+    constructor() {
+        super();
+        this.state = {
+            loggedIn: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick() {
+        this.setState({
+            loggedIn: !this.state.loggedIn
+        })
+    }
+
+    render() {
+        const buttonText = this.state.loggedIn? 'Logged In' : 'Logged out';
+
+        return (
+            <Fragment>
+                {this.props.isLoading ?
+                    <h1>Loading...</h1> :
+                    <h1>Some important info</h1>
+                }
+                <button onClick={this.handleClick}>
+                    {buttonText}
+                </button>
+            </Fragment>
+        )
+    }
 }
 
 export default Conditional;
