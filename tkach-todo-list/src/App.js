fb9ac36b26d64a1998d7ec7b3196b628960fcbb0
Влад Ткач <vlad.tkach@asmbrain.com>
2019-04-19 09:27:08 +0300

    create new branch for meme generator

--- tkach-todo-list/src/App.js
+++ tkach-todo-list/src/App.js
@@ -1,16 +1,10 @@
 import React, {Fragment} from 'react';
 
-import Navbar from './components/Navbar';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
 
 function App() {
     return(
         <Fragment>
-            <Navbar/>
-            <MainContent/>
-            {/*<Footer/>*/}
+
         </Fragment>
     )
 }
