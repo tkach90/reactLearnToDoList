e1739351f61ffaad1f9a5f51dc1a5927ad20466d
Влад Ткач <vlad.tkach@asmbrain.com>
2019-04-19 08:51:38 +0300

    finish form without checkbox functionality

--- tkach-todo-list/src/components/MainContent.js
+++ tkach-todo-list/src/components/MainContent.js
@@ -1,23 +1,10 @@
 import React, {Fragment, PureComponent} from 'react';
-import styled from 'styled-components';
 
 import Item from './Item';
 import todoData from './ToDoData';
-
+import FormContainer from './form/FormContainer';
 // import StateChanging from './statetraining';
 import Conditional from './Conditional';
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    margin: 2rem auto;
-    width: 30vw;
-    
-    select {
-        margin: 1rem 1rem 1rem 0;
-    }
-`
-
 
 class MainContent extends PureComponent {
     constructor () {
@@ -30,15 +17,8 @@ class MainContent extends PureComponent {
                 gender: 'male',
             },
             loading: false,
-            firstName: '',
-            lastName: '',
-            gender: '',
-            diet: ['vegetarian', 'kosher', 'lactose free']
-
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleFormChange = this.handleFormChange.bind(this);
-        this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
 
     componentDidMount() {
@@ -72,28 +52,6 @@ class MainContent extends PureComponent {
                 }
             }
         );
-    }
-
-    handleFormChange(event) {
-        const { value, name, type, checked } = event.target;
-        type === 'checkbox' ? this.setState( {[name]: checked}) : this.setState({[value]: checked});
-
-
-        // let diet = this.state.diet;
-        this.setState(prevState => {
-                const updateState = prevState.this.state.diet.map(item => {
-                        return {
-                            diet: updateState
-                        }
-                    }
-                )
-            }
-        )
-
-    }
-
-    handleSubmitForm() {
-
     }
 
     render() {
@@ -131,102 +89,7 @@ class MainContent extends PureComponent {
                         {text}
                     </Fragment>
 
-                    <Form>
-                        <input
-                            type="text"
-                            value={this.state.firstName}
-                            name='firstName'
-                            placeholder='First Name'
-                            onChange={this.handleFormChange}
-                        />
-                        <br/>
-                        <input
-                            type="text"
-                            value={this.state.lastName}
-                            name='lastName'
-                            placeholder='Last Name'
-                            onChange={this.handleFormChange}
-                        />
-                        <br/>
-                        <input
-                            type="number"
-                            value={this.state.age}
-                            name='age'
-                            placeholder='Age'
-                            onChange={this.handleFormChange}
-                        />
-                        <br/>
-                        <label>
-                            <input
-                                type="radio"
-                                value='male'
-                                name='gender'
-                                placeholder='Gender'
-                                checked={this.state.gender === 'male'}
-                                onChange={this.handleFormChange}
-                            />
-                            Male
-                        </label>
-                        <br/>
-                        <label>
-                            <input
-                                type="radio"
-                                value='female'
-                                name='gender'
-                                placeholder='Gender'
-                                checked={this.state.gender === 'female'}
-                                onChange={this.handleFormChange}
-                            />
-                            Female
-                        </label>
-                        <br/>
-                        <label>
-                            <select name="destination">
-                                <option value="this.state.destination">Dubai</option>
-                                <option value="this.state.destination">Amsterdam</option>
-                                <option value="this.state.destination">Singapore</option>
-                            </select>
-                            Destination
-                        </label>
-                        <br/>
-                        <label>
-                            <input
-                                type="checkbox"
-                                value={this.state.diet}
-                                name='vegetarian'
-                                placeholder='Diet'
-                                checked={this.state.name === 'vegetarian'}
-                                onChange={this.handleFormChange}
-                            />
-                            vegetarian
-                        </label>
-                        <br/>
-                        <label>
-                            <input
-                                type="checkbox"
-                                value={this.state.diet}
-                                name='kosher'
-                                placeholder='Diet'
-                                checked={this.state === 'kosher'}
-                                onChange={this.handleFormChange}
-                            />
-                            kosher
-                        </label>
-                        <br/>
-                        <label>
-                            <input
-                                type="checkbox"
-                                value={this.state.diet}
-                                name='lactose free'
-                                placeholder='Diet'
-                                checked={this.state === 'lactose free'}
-                                onChange={this.handleFormChange}
-                            />
-                            lactose free
-                        </label>
-                        <br/>
-                        <button>Submit</button>
-                    </Form>
+                    <FormContainer/>
                 </main>
             </Fragment>
         )
