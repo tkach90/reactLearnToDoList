f646e679d62caeb3feb55ef366b4ccb8d5e1749e
Влад Ткач <vlad.tkach@asmbrain.com>
2019-04-24 10:44:25 +0300

    add functionality for generating and displaying mem text and images

--- tkach-todo-list/src/MemeGenerator.js
+++ tkach-todo-list/src/MemeGenerator.js
@@ -1,4 +1,25 @@
 import React, { Fragment, PureComponent } from 'react';
+import styled from 'styled-components';
+
+const Meme = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    color: #fff;
+
+      h2:first-of-type {
+        position: absolute;
+        top: 1rem;
+        left: 50%;
+        transform: translate(-50%, -50%); 
+      }
+      h2:nth-of-type(2) {
+        position: absolute;
+        bottom: 1rem;
+        left: 50%;
+        transform: translate(-50%, -50%); 
+      }
+`;
 
 class MemeGenerator extends PureComponent {
     constructor() {
@@ -8,7 +29,9 @@ class MemeGenerator extends PureComponent {
             bottomText: '',
             randomImage: 'http://i.imgflip.com/1bij.jpg',
             allMemeImgs: [],
-        }
+        };
+
+        this.handleChangeForm = this.handleChangeForm.bind(this);
     }
 
     componentDidMount() {
@@ -23,12 +46,40 @@ class MemeGenerator extends PureComponent {
             });
     }
 
+    handleChangeForm(event) {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
     render() {
         return (
             <Fragment>
-                <form className='meme-form'>
-
+                <form>
+                    <input
+                        type="text"
+                        placeholder='Bottom text'
+                        name='topText'
+                        value={this.state.topText}
+                        onChange={this.handleChangeForm}
+                    />
+                    <input
+                        type="text"
+                        placeholder='Top text'
+                        name='bottomText'
+                        value={this.state.bottomText}
+                        onChange={this.handleChangeForm}
+                    />
+                    <button>Generate</button>
                 </form>
+
+                <Meme>
+                    <img src={this.state.randomImage} alt="meme"/>
+                    <h2>{this.state.topText}</h2>
+                    <h2>{this.state.bottomText}</h2>
+                </Meme>
             </Fragment>
         )
     }
